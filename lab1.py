07f6b2c038d06fd38a1e8cd5a4998d9a3cbfc32a
Erik McCutchen <erikmccutchen@gmail.com>
2024-01-18 17:30:14 -0800

    trying to get cbc to work now!

--- lab1.py
+++ lab1.py
@@ -1,13 +1,18 @@
 from Crypto.Cipher import AES
 from Crypto.Cipher import ARC4
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad, unpad
 
 def main():
     data = 'this is the wireless security lab'
     aes_encrypt(data)
     rc4_encrypt(data)
-    # pattern_preservation_data = 'this is good, very good!'
-    # ecb_pattern_preservation_check(pattern_preservation_data)
+    pattern_preservation_data1 = 'good good good good bad'
+    pattern_preservation_data2 = 'good good good good'
+    error_propagation_data = 'i hope this does not propagate!'
+    ecb_pattern_preservation_check(pattern_preservation_data1, pattern_preservation_data2)
+    ecb_error_propagation_check(error_propagation_data)
+    cbc_pattern_preservation_check(pattern_preservation_data1, pattern_preservation_data2)
+    cbc_error_propagation_check(error_propagation_data)
 
 def aes_encrypt(data): 
     # is this key 128 1s?
@@ -28,10 +33,10 @@ def aes_brute_force(data, ciphertext):
     iterator = int(bitstring, 2)
     while iterator > 0:
         key = iterator.to_bytes(16, byteorder='big')
-        data3 = pad(bytes(data, 'utf-8'), AES.block_size)
+        padded_data = pad(bytes(data, 'utf-8'), AES.block_size)
         cipher = AES.new(key, AES.MODE_ECB)
-        ciphertext2 = cipher.encrypt(data3)
-        if ciphertext == ciphertext2:
+        ciphertext_check = cipher.encrypt(padded_data)
+        if ciphertext == ciphertext_check:
             print('AES Key Found: ' + str(key))
             break
         iterator -= 1        
@@ -61,13 +66,63 @@ def rc4_brute_force(data, ciphertext):
             break
         iterator -= 1
 
-def ecb_pattern_preservation_check(data):
+def ecb_pattern_preservation_check(data1, data2):
+    bitstring1 = '1' * 128
+    key1 = int(bitstring1, 2).to_bytes(16, byteorder='big')
+    initial_data1 = pad(bytes(data1, 'utf-8'), AES.block_size)
+    cipher1 = AES.new(key1, AES.MODE_ECB)
+    ciphertext1 = cipher1.encrypt(initial_data1)
+    print('ECB Pattern Preservation P Ciphertext 1: ' + str(ciphertext1))
+    bitstring2 = '1' * 128
+    key2 = int(bitstring2, 2).to_bytes(16, byteorder='big')
+    initial_data2 = pad(bytes(data2, 'utf-8'), AES.block_size)
+    cipher2 = AES.new(key2, AES.MODE_ECB)
+    ciphertext2 = cipher2.encrypt(initial_data2)
+    print('ECB Pattern Preservation Ciphertext 2: ' + str(ciphertext2))
+
+def ecb_error_propagation_check(data):
     bitstring = '1' * 128
     key = int(bitstring, 2).to_bytes(16, byteorder='big')
-    initial_data = pad(bytes(data, 'utf-8'), AES.block_size)
+    initial_data = pad(bytearray(data, 'utf-8'), AES.block_size)
     cipher = AES.new(key, AES.MODE_ECB)
     ciphertext = cipher.encrypt(initial_data)
-    print(ciphertext)
+    print('ECB Error Propagation Ciphertext Before Change: ' + str(ciphertext))
+    ciphertext = bytearray(ciphertext)
+    ciphertext[8] = 10
+    ciphertext = bytes(ciphertext)
+    print('ECB Error Propagation Ciphertext After Change: ' + str(ciphertext))
+    decrypted_data = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    print('ECB Error Propagation Plaintext After Change: ' + str(decrypted_data))
+
+def cbc_pattern_preservation_check(data1, data2):
+    bitstring1 = '1' * 128
+    key1 = int(bitstring1, 2).to_bytes(16, byteorder='big')
+    initial_data1 = pad(bytes(data1, 'utf-8'), AES.block_size)
+    cipher1 = AES.new(key1, AES.MODE_CBC)
+    ciphertext1 = cipher1.encrypt(initial_data1)
+    print('ECB Pattern Preservation P Ciphertext 1: ' + str(ciphertext1))
+    bitstring2 = '1' * 128
+    key2 = int(bitstring2, 2).to_bytes(16, byteorder='big')
+    initial_data2 = pad(bytes(data2, 'utf-8'), AES.block_size)
+    cipher2 = AES.new(key2, AES.MODE_CBC)
+    ciphertext2 = cipher2.encrypt(initial_data2)
+    print('ECB Pattern Preservation Ciphertext 2: ' + str(ciphertext2))
+
+def cbc_error_propagation_check(data):
+    bitstring = '1' * 128
+    key = int(bitstring, 2).to_bytes(16, byteorder='big')
+    initial_data = pad(bytearray(data, 'utf-8'), AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC)
+    ciphertext = cipher.encrypt(initial_data)
+    print('ECB Error Propagation Ciphertext Before Change: ' + str(ciphertext))
+    ciphertext = bytearray(ciphertext)
+    ciphertext[8] = 10
+    ciphertext = bytes(ciphertext)
+    print('ECB Error Propagation Ciphertext After Change: ' + str(ciphertext))
+    decrypted_data = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    print('ECB Error Propagation Plaintext After Change: ' + str(decrypted_data))
+
+    
 
 
 
